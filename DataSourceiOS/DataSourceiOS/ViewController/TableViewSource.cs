a17566d41604275b1d3e9c50fd872535a2382a69
lakshmi.natarajan <lakshmi.natarajan@syncfusion.com>
2021-06-04 15:47:23 +0530

    GettingStarted sample added

---
+++ DataSourceiOS/DataSourceiOS/ViewController/TableViewSource.cs
@@ -0,0 +1,63 @@
+﻿using Foundation;
+using Syncfusion.DataSource;
+using Syncfusion.DataSource.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIKit;
+
+namespace DataSourceiOS
+{
+    #region TableViewSource
+    public class TableViewSource : UITableViewSource
+    {
+        #region Fields
+
+        DataSource dataSource;
+        #endregion
+
+        #region Constructor
+        public TableViewSource(DataSource sfDataSource)
+        {
+            dataSource = sfDataSource;
+        }
+        #endregion
+
+        #region Override methods
+        public override UITableViewCell GetCell(UITableView tableView, Foundation.NSIndexPath indexPath)
+        {
+            var item = dataSource.DisplayItems[indexPath.Row];
+            UITableViewCell cell = tableView.DequeueReusableCell("TableCell") as UITableViewCell;
+
+            if (cell == null)
+            {
+                cell = new UITableViewCell();
+            }
+
+            if (item is Contacts)
+            {
+                cell.TextLabel.MinimumFontSize = 12f;
+                cell.TextLabel.Text = (item as Contacts).ContactName;
+                cell.TextLabel.BackgroundColor = UIColor.Clear;
+                cell.TextLabel.TextAlignment = UITextAlignment.Left;
+            }
+            else if (item is GroupResult)
+            {
+                var group = item as GroupResult;
+                cell.TextLabel.Font = UIFont.BoldSystemFontOfSize(14);
+                cell.TextLabel.Text = group.Key.ToString();
+                cell.TextLabel.TextAlignment = UITextAlignment.Center;
+                cell.BackgroundColor = UIColor.Gray;
+            }
+            return cell;
+        }
+
+        public override nint RowsInSection(UITableView tableView, nint section)
+        {
+            return (nint)dataSource.DisplayItems.Count;
+        }
+        #endregion
+    }
+    #endregion
+}
---
+++ DataSourceiOS/DataSourceiOS/ViewModel/ContactsList.cs
@@ -0,0 +1,158 @@
+﻿using Foundation;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using UIKit;
+
+namespace DataSourceiOS
+{
+    #region ContactList
+    public class ContactsList : ObservableCollection<Contacts>, INotifyPropertyChanged
+    {
+        #region Constructor
+        public ContactsList()
+        {
+            foreach (var customerName in CustomerNames)
+            {
+                var contact = new Contacts(customerName);
+                this.Add(contact);
+            }
+        }
+        #endregion
+
+        #region Fields
+
+        string[] CustomerNames = new string[] {
+            "Kyle",
+            "Gina",
+            "Irene",
+            "Katie",
+            "Michael",
+            "Oscar",
+            "Ralph",
+            "Torrey",
+            "William",
+            "Bill",
+            "Daniel",
+            "Frank",
+            "Brenda",
+            "Danielle",
+            "Fiona",
+            "Howard",
+            "Jack",
+            "Larry",
+            "Holly",
+            "Jennifer",
+            "Liz",
+            "Pete",
+            "Steve",
+            "Vince",
+            "Zeke",
+            "Aiden",
+            "Jackson"    ,
+            "Mason  "    ,
+            "Liam   "    ,
+            "Jacob  "    ,
+            "Jayden "    ,
+            "Ethan  "    ,
+            "Noah   "    ,
+            "Lucas  "    ,
+            "Logan  "    ,
+            "Caleb  "    ,
+            "Caden  "    ,
+            "Jack   "    ,
+            "Ryan   "    ,
+            "Connor "    ,
+            "Michael"    ,
+            "Elijah "    ,
+            "Brayden"    ,
+            "Benjamin"   ,
+            "Nicholas"   ,
+            "Alexander"  ,
+            "William"    ,
+            "Matthew"    ,
+            "James  "    ,
+            "Landon "    ,
+            "Nathan "    ,
+            "Dylan  "    ,
+            "Evan   "    ,
+            "Luke   "    ,
+            "Andrew "    ,
+            "Gabriel"    ,
+            "Gavin  "    ,
+            "Joshua "    ,
+            "Owen   "    ,
+            "Daniel "    ,
+            "Carter "    ,
+            "Tyler  "    ,
+            "Cameron"    ,
+            "Christian"  ,
+            "Wyatt  "    ,
+            "Henry  "    ,
+            "Eli    "    ,
+            "Joseph "    ,
+            "Max    "    ,
+            "Isaac  "    ,
+            "Samuel "    ,
+            "Anthony"    ,
+            "Grayson"    ,
+            "Zachary"    ,
+            "David  "    ,
+            "Christopher",
+            "John   "    ,
+            "Isaiah "    ,
+            "Levi   "    ,
+            "Jonathan"   ,
+            "Oliver "    ,
+            "Chase  "    ,
+            "Cooper "    ,
+            "Tristan"    ,
+            "Colton "    ,
+            "Austin "    ,
+            "Colin  "    ,
+            "Charlie"    ,
+            "Dominic"    ,
+            "Parker "    ,
+            "Hunter "    ,
+            "Thomas "    ,
+            "Alex   "    ,
+            "Ian    "    ,
+            "Jordan "    ,
+            "Cole   "    ,
+            "Julian "    ,
+            "Aaron  "    ,
+            "Carson "    ,
+            "Miles  "    ,
+            "Blake  "    ,
+            "Brody  "    ,
+            "Adam   "    ,
+            "Sebastian"  ,
+            "Adrian "    ,
+            "Nolan  "    ,
+            "Sean   "    ,
+            "Riley  "    ,
+            "Bentley"    ,
+            "Xavier "    ,
+            "Hayden "    ,
+            "Jeremiah"   ,
+            "Jason  "    ,
+            "Jake   "    ,
+            "Asher  "    ,
+            "Micah  "    ,
+            "Jace   "    ,
+            "Brandon"    ,
+            "Josiah "    ,
+            "Hudson "    ,
+            "Nathaniel"  ,
+            "Bryson "    ,
+            "Ryder  "    ,
+            "Justin "    ,
+            "Bryce  "    ,
+        };
+        #endregion
+    }
+    #endregion
+}
